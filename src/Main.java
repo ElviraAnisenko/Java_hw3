80a00b51b8765490d81bbd4dbaf965f42e4b9ecf
Anisenko Elvira <elvira.anisenko@yandex.ru>
2023-09-11 15:44:25 +0300

    Add task

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
 public class Main {
+    public static void main(String[] args) {
+        int account = 112;
+        int refill = 120;
+        int bonus;
+        if (refill > 1000) {
+            bonus = refill / 100;
+        } else {
+            bonus = 0;
+        }
+        System.out.println("Количество бонусных рублей " + bonus + " рублей(ля)");
+        System.out.println("Итоговый счет " + (account + bonus + refill) + " рублей(ля)");
+    }
 }
